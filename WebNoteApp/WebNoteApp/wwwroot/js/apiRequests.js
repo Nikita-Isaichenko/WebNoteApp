9258b312d861676f4c88b5ae345345379907b92d
Nikita <nikita_isaichenk@ro.ru>
2023-10-10 01:19:56 +0700

    fix(apiRequests, site): исправлены небольшие недочеты в клиентском коде

---
+++ WebNoteApp/WebNoteApp/wwwroot/js/apiRequests.js
@@ -0,0 +1,139 @@
+// Get-запрос на получение всех записок.
+async function getNotes() {
+    console.log("getnotes");
+    var response = await fetch("/Home/GetNotes", {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (response.ok === true) {
+
+        clearTitleBox();
+
+        notes = await response.json();
+
+        for (var i = 0; i < notes.length; i++) {
+            title.appendChild(addAttributeOption(notes[i].title, notes[i].id));
+        }
+
+        if (noteId.value == ""){
+            title.options[title.options.length - 1].setAttribute("selected", true);
+        }
+
+        title.dispatchEvent(new Event('change'));
+    }
+}
+
+// Get-запрос на получение одной записи по id.
+async function getNote(e) {
+    if (e.target.value != ""){
+        var response = await fetch(`/Home/GetNote?id=${e.target.value}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        });
+    
+        if (response.ok === true) {
+            var note = await response.json();
+    
+            setValue(note);
+        }
+    }
+    else{
+        setValue(null);
+    }
+
+}
+
+// Delete-запрос для удаления записки по id.
+async function deleteNote() {
+    console.log("deletenote");
+    if (noteId.value) {
+        var response = await fetch(`/Home/DeleteNote?id=${noteId.value}`, {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (response.ok === true) {
+            noteId.value = "";
+            await getNotes();
+        }
+    }
+    else {
+        alert("Выберите запись для удаления!");
+    }
+}
+
+// Выполняет либо отправку запроса на создание, либо на изменение.
+async function saveNote() {
+    if (isCreate) {
+        await saveCreatedNote();
+    } else {
+        await saveEditedNote();
+    }
+
+    hiddenButtons();
+    reset();
+    await getNotes();
+}
+
+// Post-запрос для создания записки.
+async function saveCreatedNote() {
+    console.log("savecreatednote");
+    noteId.value = "";
+    var response = await fetch("/Home/CreateNote", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                title: schema.value,
+                description: textarea.value,
+                category: category.value,
+            }
+        )
+    });
+
+    if (response.ok === true) {
+        var message = await response.json();
+        alert(message.message);
+    }
+    else {
+        var message = await response.json();
+        alert(message.message);
+    }
+}
+
+// Put-запрос для изменения записки.
+async function saveEditedNote() {
+    console.log("saveeditnote");
+    var response = await fetch("/Home/UpdateNote", {
+        method: "PUT",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            {
+                id: noteId.value,
+                title: schema.value,
+                description: textarea.value,
+                category: category.value,
+            }
+        )
+    });
+
+    if (response.ok === true) {
+        alert("Update запрос прошел");
+    }
+}
--- WebNoteApp/WebNoteApp/wwwroot/js/site.js
+++ WebNoteApp/WebNoteApp/wwwroot/js/site.js
@@ -16,7 +16,7 @@ document.getElementById("title").addEventListener('change', getNote);
 document.getElementById("edit").addEventListener("click", editNote);
 document.getElementById("delete").addEventListener("click", deleteNote);
 
-async function editNote() {
+function editNote() {
 
     isCreate = false;
     title.setAttribute("disabled", true);
@@ -26,10 +26,9 @@ async function editNote() {
     hiddenButtons();
 }
 
-async function createNote() {
+function createNote() {
 
     isCreate = true;
-    noteId.value = -1;
     reset();
 
     title.setAttribute("disabled", true);
@@ -38,61 +37,6 @@ async function createNote() {
     schema.value = "Без названия";
 
     hiddenButtons();
-}
-
-async function saveNote() {
-
-        if (isCreate){
-            var response = await fetch("/Home/CreateNote", {
-                method: "POST",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(
-                    {
-                        title: schema.value,
-                        description: textarea.value,
-                        category: category.value,
-                    }
-                )
-            });
-    
-            if (response.ok === true){
-                var message = await response.json();
-                await alert(message.message);
-            }
-            else{
-                var message = await response.json();
-                await alert(message.message);
-            }       
-        }else{
-            var response = await fetch("/Home/UpdateNote", {
-                method: "PUT",
-                headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(
-                    {
-                        id: noteId.value,
-                        title: schema.value,
-                        description: textarea.value,
-                        category: category.value,
-                    }
-                )
-            });
-
-            if (response.ok === true){
-                alert("Update запрос прошел");
-               // var note = await response.json();
-
-            }
-        }
-        
-        hiddenButtons();
-        reset();
-        getNotes();
 }
 
 function cancelChanges() {
@@ -127,72 +71,6 @@ function reset() {
     title.removeAttribute("disabled");
 }
 
-async function deleteNote(){
-    if (noteId.value){
-        var response = await fetch(`/Home/DeleteNote?id=${noteId.value}`, {
-            method: "DELETE",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            }
-        })
-
-        if (response.ok === true){
-            getNotes();
-            
-        }
-    }
-    else{
-        alert("Выберите запись для удаления!");
-    }
-    
-}
-
-async function getNote(e) {
-    var response = await fetch(`/Home/GetNote?id=${e.target.value}`, {
-        method: "GET",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-    });
-
-    if (response.ok === true){
-        var note = await response.json();
-
-        schema.value = note.title;
-        textarea.value = note.description;
-        dateCreated.value = note.created;
-        dateModified.value = note.modified;
-        noteId.value = note.id;
-    }
-
-
-}
-
-async function getNotes() {
-    var response = await fetch("/Home/GetNotes", {
-        method: "GET",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-    });
-
-    if (response.ok === true) {
-
-        clearTitleBox();
-
-        notes = await response.json();
-
-        for (var i = 0; i < notes.length; i++) {
-            title.appendChild(addAttributeOption(notes[i].title, notes[i].id));
-        }
-
-        title.dispatchEvent(new Event('change'));
-    }
-}
-
 function clearTitleBox() {
     while (title.options.length > 0) {
         title.remove(0);
@@ -204,10 +82,29 @@ function addAttributeOption(stringTitle, Id) {
     option.value = Id;
     option.text = stringTitle;
 
-    if (noteId.value && Id == noteId.value){
+    if (noteId.value != "" && Id == noteId.value) {
         alert("Сравнение");
         option.setAttribute("selected", true);
     }
 
     return option;
 }
+
+function setValue(note) {
+    if (note != null){
+        schema.value = note.title;
+        textarea.value = note.description;
+        dateCreated.value = note.created;
+        dateModified.value = note.modified;
+        noteId.value = note.id;
+    }
+    else{
+        schema.value = "";
+        textarea.value = "";
+        dateCreated.value = "";
+        dateModified.value = "";
+        noteId.value = "";
+    }
+}
+
+
